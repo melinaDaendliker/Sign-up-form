59a01b3dc19adf08bc7577c15d5284ddeb63acac
melinaDaendliker <melina.daendliker@hotmailc.om>
2023-10-28 22:49:35 +0200

    javascript function to check if passwords are the same

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+function ansValidation () {
+  var passValue = document.getElementById("password");
+  console.log(passValue.value)
+  var confpassValue = document.getElementById("password_conf");
+  console.log(confpassValue.value)
+  var passwordError = document.getElementById("password_error");
+  if (passValue.value !== confpassValue.value) {
+    passValue.classList.add('wrong_pass')
+    confpassValue.classList.add('wrong_pass')
+    passwordError.innerText = "*passwords do not match.";
+    passwordError.style.display = 'block';
+  }
+  else {
+    passValue.classList.remove('wrong_pass')
+    confpassValue.classList.remove('wrong_pass')
+    passwordError.innerText = "";
+    passwordError.style.display = 'none';
+  }
+}
